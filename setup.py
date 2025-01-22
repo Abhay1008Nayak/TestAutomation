3239a72ae50265196fdd40611ee056d25dd0a57a
abhay <abhay.nayak@cubexo.io>
2025-01-22 16:46:36 +0530

    Intial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_project",  # Replace with your project name
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+    description="A sample Python project",
+)
